dc0b4a46cc9e3cadb908f6c113ce9d73f175cfb3
hugo <hugo@gauss>
2016-02-15 16:05:19 +0000

    alteração novos nomes

--- src/github/teste/GithubTeste.java
+++ src/github/teste/GithubTeste.java
@@ -17,7 +17,7 @@ public class GithubTeste
      */
     public static void main(String[] args)
     {
-        System.out.println("hugo davide");
+        System.out.println("hugo davide silvia andre");
     }
     
 }
